144fb9903ebf641db477ab1a04c9b3075fca87f0
G.YUVA KISHORE REDDY <156158020+Kishore276@users.noreply.github.com>
2025-03-29 09:28:20 +0530

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View } from 'react-native';
+import { PatientDashboard } from './src/screens/PatientDashboard';
+
+export default function App() {
+  return (
+    <View style={{ flex: 1 }}>
+      <PatientDashboard />
+    </View>
+  );
+} 

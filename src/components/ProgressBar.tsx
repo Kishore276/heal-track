5622ca6048adbf8f6f87aededde8193f6232bd96
G.YUVA KISHORE REDDY <g.yuvakishorereddy@gmail.com>
2025-10-26 19:43:23 +0530

    Updated file

---
+++ src/components/ProgressBar.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+interface ProgressBarProps {
+  progress: number;
+}
+
+export const ProgressBar: React.FC<ProgressBarProps> = ({ progress }) => {
+  return (
+    <div className="w-full bg-gray-200 rounded-full h-2.5">
+      {/* eslint-disable-next-line */}
+      <div
+        className="bg-green-600 h-2.5 rounded-full transition-all duration-300"
+        style={{ width: `${progress}%` }}
+      />
+    </div>
+  );
+};
